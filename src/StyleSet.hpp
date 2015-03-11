d65528344b48d527ace962cab88b1bdd70b32af2
gck <gck@ableton.com>
2015-03-11 14:51:07 +0100

    Update copyright notice in header

--- src/StyleSet.hpp
+++ src/StyleSet.hpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014 Ableton AG, Berlin
+Copyright (c) 2014-15 Ableton AG, Berlin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
