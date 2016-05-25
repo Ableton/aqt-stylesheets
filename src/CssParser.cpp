4b0c40b4f478c7a6d5df6e6f04c1966ca0634e26
gck <gck@ableton.com>
2016-05-25 09:44:47 +0200

    Add alternative CSS parser implementation using the cpp-peglib parser

--- src/CssParser.cpp
+++ src/CssParser.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014-15 Ableton AG, Berlin
+Copyright (c) 2014-16 Ableton AG, Berlin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -25,14 +25,11 @@ THE SOFTWARE.
 #include "Warnings.hpp"
 
 SUPPRESS_WARNINGS
-#include <boost/fusion/include/adapt_struct.hpp>
-#include <boost/spirit/include/phoenix_fusion.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_stl.hpp>
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/support_line_pos_iterator.hpp>
+#include "cpp-peglib/peglib.h"
+#include <boost/variant/get.hpp>
 RESTORE_WARNINGS
 
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -56,208 +53,6 @@ std::string loadFileIntoString(const std::string& path)
 
 } // anon namespace
 
-// this must be outside of the anon namespace
-// clang-format off
-BOOST_FUSION_ADAPT_STRUCT(
-  aqt::stylesheets::PropertySpec,
-  (std::string, name)
-  (aqt::stylesheets::PropertyValues, values)
-  )
-
-BOOST_FUSION_ADAPT_STRUCT(
-  aqt::stylesheets::PropertySpecSet,
-  (std::vector<aqt::stylesheets::Selector>, selectors)
-  (std::vector<aqt::stylesheets::PropertySpec>, properties)
-  )
-
-BOOST_FUSION_ADAPT_STRUCT(
-  aqt::stylesheets::FontFaceDecl,
-  (std::string, url)
-  )
-
-BOOST_FUSION_ADAPT_STRUCT(
-  aqt::stylesheets::StyleSheet,
-  (std::vector<aqt::stylesheets::PropertySpecSet>, propsets)
-  (std::vector<aqt::stylesheets::FontFaceDecl>, fontfaces)
-  )
-
-BOOST_FUSION_ADAPT_STRUCT(
-  aqt::stylesheets::Expression,
-  (std::string, name)
-  (std::vector<std::string>, args)
-  )
-// clang-format on
-
-namespace
-{
-
-namespace phoenix = boost::phoenix;
-namespace qi = boost::spirit::qi;
-namespace ascii = boost::spirit::ascii;
-
-template <typename It>
-class LocationAnnotator
-{
-  const It mFirst;
-
-public:
-  // to make phoenix::function happy
-  template <typename>
-  struct result {
-    using type = void;
-  };
-
-  LocationAnnotator(It first)
-    : mFirst(first)
-  {
-  }
-
-  template <typename ThingWithLocation>
-  void operator()(ThingWithLocation& thing, It iter) const
-  {
-    thing.mSourceLoc.mByteOfs = static_cast<int>(std::distance(mFirst, iter));
-    thing.mSourceLoc.mLine = static_cast<int>(get_line(iter));
-
-    // The column offset is always off by one. Why? It is a mystery.
-    thing.mSourceLoc.mColumn = static_cast<int>(get_column(mFirst, iter)) - 1;
-  }
-};
-
-template <typename Iterator>
-struct StyleSheetGrammar
-  : qi::grammar<Iterator, aqt::stylesheets::StyleSheet(), ascii::space_type> {
-  StyleSheetGrammar(Iterator first)
-    : StyleSheetGrammar::base_type(stylesheet, "stylesheet")
-    , locationAnnotator(first)
-  {
-    using qi::lit;
-    using qi::lexeme;
-    using qi::omit;
-    using ascii::char_;
-    using ascii::string;
-    using namespace qi::labels;
-    using phoenix::at_c;
-    using phoenix::push_back;
-    using boost::spirit::eol;
-
-    // clang-format off
-    cpp_comment     = "//" >> omit[*(char_ - eol) >> eol];
-    c_comment       = "/*" >> omit[*(char_ - "*/")] >> "*/";
-    comment         = cpp_comment | c_comment;
-
-    identifier      = +(qi::alnum | char_('-'));
-    dot_identifier  = char_(".") >> identifier;
-    child_sel       = char_(">");
-    sel_separator   = char_(",");
-    sel_part        = +(dot_identifier | identifier)[push_back(_val, _1)];
-    child_sel_part  = (child_sel)[push_back(_val, _1)];
-    selector        = +(sel_part | child_sel_part)[push_back(_val, _1)];
-
-    quoted_string   = lexeme['"' >> +(char_ - '"') >> '"']
-                      | lexeme['\'' >> +(char_ - '\'') >> '\''];
-    color           = char_('#') >> +(qi::xdigit);
-    number          = +(qi::digit | char_('-') | char_('.')) >> *char_('%');
-
-    atom_value       = quoted_string
-                      | number
-                      | color
-                      | identifier;
-
-    args            = atom_value [push_back(_val, _1)]
-                      >> *(lit(',') > atom_value [push_back(_val, _1)]);
-    expression      = identifier[at_c<0>(_val) = _1]
-                      >> lit('(') >> *(args[at_c<1>(_val) = _1]) >> lit(')');
-
-    value           = quoted_string
-                      | number
-                      | color
-                      | expression
-                      | identifier;
-    values          = value [push_back(_val, _1)]
-                      >> *(lit(',') > value [push_back(_val, _1)]);
-
-    value_pair      = identifier[at_c<0>(_val) = _1]
-                      >> lit(':') >> values[at_c<1>(_val) = _1]
-                      >> -lit(';');
-
-    propset         = selector [push_back(at_c<0>(_val), _1)]
-                      >> *((sel_separator >> selector[push_back(at_c<0>(_val), _1)])
-                            | comment)
-                      >> '{'
-                      >> *(value_pair [push_back(at_c<1>(_val), _1)]
-                           | comment)
-                      >> '}';
-
-    fontfacedecl    = "@font-face" >> -comment
-                      >> lit('{') >> -comment
-                      >> lit("src") >> lit(':') >> -comment
-                      >> lit("url") >> lit('(')
-                                    >> (identifier | quoted_string)[at_c<0>(_val) = _1]
-                      >> lit(')') >> -lit(';') >> -comment
-                      >> lit('}');
-
-    stylesheet      = *(propset[push_back(at_c<0>(_val), _1)]
-                        | fontfacedecl[push_back(at_c<1>(_val), _1)]
-                        | comment);
-    // clang-format on
-
-    // give proper names to the rules for error printing
-    c_comment.name("comment");
-    color.name("color");
-    comment.name("comment");
-    cpp_comment.name("comment");
-    fontfacedecl.name("fontface");
-    identifier.name("identifier");
-    dot_identifier.name("dot_identifier");
-    propset.name("propset");
-    quoted_string.name("string");
-    stylesheet.name("stylesheet");
-    sel_separator.name("selector separator");
-    sel_part.name("selector part");
-    selector.name("selector");
-    values.name("value");
-    number.name("number");
-    atom_value.name("atomic value");
-    value.name("value");
-    value_pair.name("key-value-pair");
-    child_sel.name("child");
-    args.name("args");
-    expression.name("expression");
-
-    on_success(propset, locationAnnotator(_val, _1));
-    on_success(value_pair, locationAnnotator(_val, _1));
-  }
-
-  phoenix::function<LocationAnnotator<Iterator>> locationAnnotator;
-
-  qi::rule<Iterator, std::string()> identifier;
-  qi::rule<Iterator, std::string()> dot_identifier;
-  qi::rule<Iterator, std::string()> sel_separator;
-  qi::rule<Iterator, std::string()> child_sel;
-  qi::rule<Iterator, aqt::stylesheets::SelectorParts()> child_sel_part;
-  qi::rule<Iterator, aqt::stylesheets::SelectorParts()> sel_part;
-  qi::rule<Iterator, aqt::stylesheets::Selector(), ascii::space_type> selector;
-  qi::rule<Iterator, std::string()> color;
-  qi::rule<Iterator, std::string()> number;
-
-  qi::rule<Iterator, std::string(), ascii::space_type> quoted_string;
-  qi::rule<Iterator, aqt::stylesheets::PropertySpec(), ascii::space_type> value_pair;
-  qi::rule<Iterator, std::string(), ascii::space_type> atom_value;
-  qi::rule<Iterator, aqt::stylesheets::PropertyValue(), ascii::space_type> value;
-  qi::rule<Iterator, aqt::stylesheets::PropertyValues(), ascii::space_type> values;
-  qi::rule<Iterator, std::vector<std::string>(), ascii::space_type> args;
-  qi::rule<Iterator, aqt::stylesheets::PropertySpecSet(), ascii::space_type> propset;
-  qi::rule<Iterator, aqt::stylesheets::FontFaceDecl(), ascii::space_type> fontfacedecl;
-  qi::rule<Iterator, aqt::stylesheets::StyleSheet(), ascii::space_type> stylesheet;
-  qi::rule<Iterator, aqt::stylesheets::Expression(), ascii::space_type> expression;
-
-  qi::rule<Iterator, boost::spirit::unused_type> cpp_comment;
-  qi::rule<Iterator, boost::spirit::unused_type> c_comment;
-  qi::rule<Iterator, boost::spirit::unused_type> comment;
-};
-
-} // anon namespace
-
 namespace aqt
 {
 namespace stylesheets
@@ -267,31 +62,173 @@ StyleSheet parseStdString(const std::string& data)
 {
   StyleSheet stylesheet;
 
-  using source_iterator = boost::spirit::line_pos_iterator<std::string::const_iterator>;
-  using StrStyleSheetGrammar = StyleSheetGrammar<source_iterator>;
+  auto syntax = R"(
+    # Grammar for CSS...
 
-  auto iter = source_iterator{data.begin()};
-  auto end = source_iterator{data.end()};
-  StrStyleSheetGrammar styleGrammar(iter);
+    STYLESHEET      <- ( _ ( PROPSET / FONTFACEDECL / Comment ) )* _ EndOfFile
+    FONTFACEDECL    <- '@font-face' _ '{' _
+                           'src' _ ':' _
+                           'url' _ '(' _ (IDENTIFIER / STRING) _ ')' ( _ ';')? _
+                       '}'
+    PROPSET         <- SELECTORS _ '{' _ VALUE_PAIRS _ '}'
 
-  bool retval =
-    phrase_parse(iter, end, styleGrammar, boost::spirit::ascii::space, stylesheet);
 
-  if (retval && iter == end) {
-    return stylesheet;
-  }
+    SELECTORS       <- SELECTOR ( _ ',' _ (SELECTOR / Comment) )*
+    SELECTOR        <- SEL_ID ( _ (CHILD_SEL / SEL_ID ))*
+    SEL_ID          <- (DOT_IDENTIFIER / IDENTIFIER)+
+    CHILD_SEL       <- < '>' >
 
-  if (iter != end) {
-    auto errorOfs = std::distance(source_iterator{data.begin()}, iter);
-    auto restLen = std::distance(iter, end);
+    VALUE_PAIRS     <- ((VALUE_PAIR / Comment) ( _ (VALUE_PAIR / Comment) )* )?
 
-    auto errorText = restLen > 128 ? data.substr(size_t(errorOfs), 128) + "..."
-                                   : data.substr(size_t(errorOfs), std::string::npos);
+    VALUE_PAIR      <- IDENTIFIER _ ':' _ VALUES ( _ ';')?
 
-    throw ParseException("Found unexpected tokens", errorText);
-  } else {
-    throw ParseException("Unknown error");
-  }
+    VALUES          <- VALUE ( _ ',' _ VALUE )*
+    VALUE           <- STRING_VAL / NUMBER_VAL / COLOR_VAL / EXPRESSION / SYMBOL_VAL
+    STRING_VAL      <- STRING
+    NUMBER_VAL      <- NUMBER
+    COLOR_VAL       <- COLOR
+    SYMBOL_VAL      <- IDENTIFIER
+
+    EXPRESSION      <- IDENTIFIER _ '(' (_ ARGS)? _ ')'
+    ARGS            <- ATOM_VALUE ( _ ',' _ ATOM_VALUE )*
+    ATOM_VALUE      <- STRING / NUMBER / COLOR / IDENTIFIER
+
+    DOT_IDENTIFIER  <- < '.' IdentInitChar IdentChar* >
+    IDENTIFIER      <- < IdentInitChar IdentChar* >
+
+    COLOR           <- < '#' [0-9a-fA-F]+ >
+    NUMBER          <- < '-'? [0-9]+ ('.' [0-9]+)? '%'? >
+    STRING          <- (['] < (!['] .)* > [']) / ([\"] < (![\"] .)* > [\"])
+
+    ~_              <-  (WhiteSpace / End)*
+
+    WhiteSpace      <-  SpaceChar / Comment
+    End             <-  EndOfLine / EndOfFile
+    Comment         <-  BlockComment / LineComment
+    SpaceChar       <-  ' ' / '\t'
+    EndOfLine       <-  '\r\n' / '\n' / '\r'
+    EndOfFile       <-  !.
+    IdentInitChar   <-  [a-zA-Z_]
+    IdentChar       <-  [-a-zA-Z0-9_]
+    BlockComment    <-  '/*' (!'*/' .)* '*/'
+    LineComment     <-  '//' (!End .)* End
+  )";
+
+  int line = 0;
+  peg::parser parser;
+  auto retv = parser.load_grammar(syntax);
+  assert(retv);
+  (void)retv;
+
+  parser.log = [&](size_t ln, size_t col, const std::string& msg) {
+    std::stringstream ss;
+    ss << ln << ":" << col << ": " << msg;
+
+    throw ParseException(ss.str());
+  };
+
+  parser["EndOfLine"] = [&line](const peg::SemanticValues&) { line++; };
+
+  parser["FONTFACEDECL"] = [&stylesheet](const peg::SemanticValues& sv) {
+    stylesheet.fontfaces.push_back({sv[0].get<std::string>()});
+  };
+
+  parser["PROPSET"] = [&stylesheet](const peg::SemanticValues& sv) {
+    PropertySpecSet set;
+    set.selectors = sv[0].get<std::vector<Selector> >();
+    set.properties = sv[1].get<std::vector<PropertySpec> >();
+    stylesheet.propsets.emplace_back(std::move(set));
+  };
+
+  parser["SELECTORS"] = [](const peg::SemanticValues& sv) {
+    std::vector<Selector> sels;
+    for (const auto& val : sv) {
+      sels.emplace_back(val.get<Selector>());
+    }
+    return sels;
+  };
+
+  parser["SELECTOR"] = [](const peg::SemanticValues& sv) {
+    Selector sel;
+    for (const auto& val : sv) {
+      sel.emplace_back(val.get<SelectorParts>());
+    }
+    return sel;
+  };
+
+  parser["SEL_ID"] = [](const peg::SemanticValues& sv) {
+    SelectorParts selp;
+    for (const auto& val : sv) {
+      selp.emplace_back(val.get<std::string>());
+    }
+    return selp;
+  };
+
+  parser["CHILD_SEL"] =
+    [](const peg::SemanticValues& sv) { return SelectorParts({sv.token()}); };
+
+  parser["VALUE_PAIRS"] = [](const peg::SemanticValues& sv) {
+    std::vector<PropertySpec> specs;
+    for (const auto& val : sv) {
+      specs.emplace_back(val.get<PropertySpec>());
+    }
+    return specs;
+  };
+
+  parser["VALUE_PAIR"] = [&data, &line](const peg::SemanticValues& sv) {
+    auto sl = SourceLocation(0, static_cast<int>(sv.c_str() - data.data()), line,
+                             0); // no column info
+    return PropertySpec{
+      sv[0].get<std::string>(), sv[1].get<PropertyValues>(), std::move(sl)};
+  };
+
+  parser["VALUES"] = [](const peg::SemanticValues& sv) {
+    PropertyValues values;
+    for (const auto& val : sv) {
+      values.emplace_back(val.get<PropertyValue>());
+    }
+    return values;
+  };
+
+  parser["EXPRESSION"] = [](const peg::SemanticValues& sv) {
+    if (sv.size() == 2) {
+      return PropertyValue(
+        Expression{sv[0].get<std::string>(), sv[1].get<std::vector<std::string> >()});
+    } else {
+      assert(sv.size() == 1);
+      return PropertyValue(Expression{sv[0].get<std::string>(), {}});
+    }
+  };
+
+  parser["ARGS"] = [](const peg::SemanticValues& sv) {
+    std::vector<std::string> args;
+    for (const auto& val : sv) {
+      args.emplace_back(val.get<std::string>());
+    }
+    return args;
+  };
+
+  auto str2propVal =
+    [](const peg::SemanticValues& sv) { return PropertyValue(sv[0].get<std::string>()); };
+  parser["STRING_VAL"] = str2propVal;
+  parser["NUMBER_VAL"] = str2propVal;
+  parser["COLOR_VAL"] = str2propVal;
+  parser["SYMBOL_VAL"] = str2propVal;
+
+  auto svToken = [](const peg::SemanticValues& sv) { return sv.token(); };
+  parser["STRING"] = svToken;
+  parser["NUMBER"] = svToken;
+  parser["COLOR"] = svToken;
+  parser["IDENTIFIER"] = svToken;
+  parser["DOT_IDENTIFIER"] = svToken;
+
+  // Enable packrat parsing.  NOTE(gck) For the CSS syntax above this doesn't
+  // make sense; without it parsing is much faster.
+  //parser.enable_packrat_parsing();
+
+  parser.parse_n(data.data(), data.size());
+
+  return stylesheet;
 }
 
 StyleSheet parseString(const QString& data)
